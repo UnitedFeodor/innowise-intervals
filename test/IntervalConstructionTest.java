12d0ada3142a61923a66abf0c43aac8693b80985
Feodor Kachinsky <fedor15zil@gmail.com>
2023-03-11 20:57:07 +0300

    refactoring

--- test/IntervalConstructionTest.java
+++ test/IntervalConstructionTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.innowise.Intervals.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @TestInstance(Lifecycle.PER_CLASS)
@@ -17,33 +18,23 @@ public class IntervalConstructionTest {
     List<String> exprectedReturnsList;
     @BeforeAll
     void initParamsArray() {
-        /*
-        |['M2', 'C', 'asc']|D|
-        |['P5', 'B', 'asc']|F#|
-        |['m2', 'Bb', 'dsc']|A|
-        |['M3', 'Cb', 'dsc']|Abb|
-        |['P4', 'G#', 'dsc']|D#|
-        |['m3', 'B', 'dsc']|G#|
-        |['m2', 'Fb', 'asc']|Gbb|
-        |['M2', 'E#', 'dsc']|D#|
-        |['P4', 'E', 'dsc']|B|
-        |['m2', 'D#', 'asc']|E|
-        |['M7', 'G', 'asc']|F#|
-         */
+
         inputArraysList = Arrays.asList(
-                new String[]{"M2", "C", "asc"},
-                new String[]{"P5", "B", "asc"},
-                new String[]{"m2", "Bb", "dsc"},
-                new String[]{"M3", "Cb", "dsc"},
-                new String[]{"P4", "G#", "dsc"},
-                new String[]{"m3", "B", "dsc"},
-                new String[]{"m2", "Fb", "asc"},
-                new String[]{"M2", "E#", "dsc"},
-                new String[]{"P4", "E", "dsc"},
-                new String[]{"m2", "D#", "asc"},
-                new String[]{"M7", "G", "asc"}
+                new String[]{INTERVAL_MAJ_2, "C", ORDER_ASC},
+                new String[]{INTERVAL_PERF_5, "B", ORDER_ASC},
+                new String[]{INTERVAL_MIN_2, "Bb", ORDER_DSC},
+                new String[]{INTERVAL_MAJ_3, "Cb", ORDER_DSC},
+                new String[]{INTERVAL_PERF_4, "G#", ORDER_DSC},
+                new String[]{INTERVAL_MIN_3, "B", ORDER_DSC},
+                new String[]{INTERVAL_MIN_2, "Fb", ORDER_ASC},
+                new String[]{INTERVAL_MAJ_2, "E#", ORDER_DSC},
+                new String[]{INTERVAL_PERF_4, "E", ORDER_DSC},
+                new String[]{INTERVAL_MIN_2, "D#", ORDER_ASC},
+                new String[]{INTERVAL_MAJ_7, "G", ORDER_ASC},
+                new String[]{INTERVAL_PERF_8,"A",ORDER_DSC}
 
         );
+
         exprectedReturnsList = Arrays.asList(
                 "D",
                 "F#",
@@ -55,24 +46,23 @@ public class IntervalConstructionTest {
                 "D#",
                 "B",
                 "E",
-                "F#"
+                "F#",
+                "A"
         );
     }
 
     @Test
     void intervalConstructionReturns() {
         for (int i = 0; i < inputArraysList.size(); i++) {
+
             String actualOutput = Intervals.intervalConstruction(inputArraysList.get(i));
             String expectedOutput = exprectedReturnsList.get(i);
-            //System.out.println(Arrays.toString(inputArraysList.get(i)) +" actual: "+ actualOutput + " expected: " + expectedOutput);
 
             Assertions.assertEquals(
                     expectedOutput,
                     actualOutput,
                     "intervalConstruction for " + Arrays.toString(inputArraysList.get(i))
             );
-
-
         }
 
     }
@@ -84,14 +74,14 @@ public class IntervalConstructionTest {
                 () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[1])),
                 () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[4])),
                 () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{"","D"})),
-                () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{"P4","","asc"})),
-                () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{"m2","C",""})),
+                () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{INTERVAL_PERF_4,"",ORDER_ASC})),
+                () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{INTERVAL_MIN_2,"C",""})),
                 () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{null,null,null})),
-                () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{"M3",null,null})),
-                () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{"m6","A",null})),
+                () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{INTERVAL_MAJ_3,null,null})),
+                () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{INTERVAL_MIN_6,"A",null})),
                 () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{"that one","A"})),
-                () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{"M6","H"})),
-                () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{"P5","D","i don't care"}))
+                () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{INTERVAL_MAJ_6,"H"})),
+                () -> assertThrows(IllegalArgumentException.class, () -> Intervals.intervalConstruction(new String[]{INTERVAL_PERF_5,"D","order"}))
         );
     }
 }
